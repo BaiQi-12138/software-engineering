57038852cb5117920a3aeee44c5822ddc96d5e8e
BaiQi-12138 <70877591+BaiQi-12138@users.noreply.github.com>
2020-09-15 18:28:22 +0800

    Update Example01.java

--- Example01.java
+++ Example01.java
@@ -1,28 +1,16 @@
+import java.util.Arrays;
+
 public class Example01 {
-	public static void main(String[] args) {
-		int[] arr={12,6,4,8,3};
-		//1.冒泡排序前。县循环打印数组元素
-		for (int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();				//用于换行
-		//2.进行冒泡排序
-		//2.1外层循环定义第一轮需要比较的轮数（两数对比，要比较n-1轮）
-		for(int i=1;i<arr.length;i++){
-			//2.2内层循环定义第一轮需要比较两个数
-			for(int j=0;j<arr.length-i;j++){
-				if(arr[j]>arr[j+1]){	//比较相邻元素
-					//下面的三行代码用于相邻两个元素交换
-					int temp=arr[j];
-					arr[j]=arr[j+1];
-					arr[j+1]=temp;
+	public static void mpPx(int arr[]){
+		for (int i = 0; i <= arr.length - 1; i++){
+			for (int j = 0; j < arr.length - 1 - i;j++){
+				if (arr[j] > arr[j+1]){
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
 				}
 			}
 		}
-		//3.完成冒泡排序后，再次循环打印数组元素
-		for(int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
-		}
 	}
 }
-//使用冒泡排序对数组元素12、6、4、8、3进行从小到大排序
+//用eclipse打包冒泡排序算法方法
